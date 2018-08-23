d43989b49453be51efaddd2b4e35bdc5a9f20063
Brad McCollum <btmcco8@gmail.com>
2018-08-23 18:41:59 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,13 +3,13 @@ require 'pry'
 class Cat
   # code goes here
 
-  attr_reader :name, :owner
+  attr_reader :name, :owner, :mood
 
   def initialize(name)
     @name = name
+    @mood = mood
   end
 
-  def mood(mood)
-  end
+
 end
 # binding.pry
