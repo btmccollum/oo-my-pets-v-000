a586270f1e4065d6123d4c9c110823f13da6945e
Brad McCollum <btmcco8@gmail.com>
2018-08-23 18:36:33 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,10 +1,10 @@
 class Fish
   # code goes here
   attr_accessor :name
-  
+
     def initialize(name)
       @name = name
-      
+
     end
-    
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,5 +1,11 @@
 class Owner
   # code goes here
   attr_accessor :name
+  
+    def initialize(name)
+      @name = name
+      
+    end
+    
 
 end
